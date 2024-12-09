b23f780e81bbc8cb1e47178672e1e7e50ea9b474
CyanPC <cyan.pereira@gmail.com>
2024-12-09 17:25:53 -0300

    1st commit

---
+++ src/test/java/com/cyanpc/task_scheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyanpc.task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
